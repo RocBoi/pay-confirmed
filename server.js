df78a7ce5d849abd3c9e2baf8890e88d73753a61
RocBoi <rocboiquezworld2@gmail.com>
2025-06-01 17:44:40 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+// server.js
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const connectDB = require('./config/db'); // MongoDB connection
+const routes = require('./routes');       // Import route index
+
+// Init app
+const app = express();
+
+// Connect to DB
+connectDB();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+// API Routes
+app.use('/api', routes);
+
+// Export app for index.js
+module.exports = app;
